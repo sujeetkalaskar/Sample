9cd8fc8fa312c02e903bb92d9d078bfe725f5ef3
Sujeet Kalaskar <sujeetkalaskar@gmail.com>
2024-02-18 22:14:50 +0530

    Update polyfills.ts
    Test

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -46,7 +46,7 @@
 
 
 /**
- * Web Animations `@angular/platform-browser/animations` This is sujeet
+ * Web Animations `@angular/platform-browser/animations` This is sujeet Kalaskar
  * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
  * Standard animation support in Angular DOES NOT require any polyfills (as of Angular 6.0).
  */
